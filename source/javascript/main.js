3769fef286b0bafe904a0bcabe8cf6aec86599eb
beatriz-fraga <beeh.m.frg@gmail.com>
2022-05-09 17:09:29 -0300

    updated

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,43 +1,81 @@
 function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
-
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
 }
 
-onScroll()
-function showNavOnScroll() {
-    if(scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+function activateMenuAtCurrentSection(section) {
+  /* Linha Alvo */
+  const targetLine = scrollY + innerHeight / 2;
 
+  /* Verificar se a seção passou da linha */
+  /* Quais dados eu preciso? */
+
+  /* topo da seção */
+  const sectionTop = section.offsetTop;
+  /* altura da seção */
+  const sectionHeight = section.offsetHeight;
+
+  /* O topo da seção chegou ou ultrapassou a linha alvo */
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  /* Informações de dados e lógica */
+  console.log(sectionTopReachOrPassedTargetline);
+  /*verificar se a base está abaixo da linha alvo*/
+  /*Quais dados eu preciso?*/
+
+  /* Onde a seção termina? */
+  const sectionEndsAt = sectionTop + sectionHeight;
+  /* O final da seção passou da linha alvo? */
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+  console.log(sectionEndPassedTargetline);
+
+  /* limites da seção */
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove('active');
+  if (sectionBoundaries) {
+    menuElement.classList.add('active');
+  }
+}
+
+onScroll();
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
 }
 
 function showBackToTopButtonOnScroll() {
-    if(scrollY > 550) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
-
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
 }
 
-window.addEventListener('scroll', onScroll)
-
-
+window.addEventListener('scroll', onScroll);
 
 function openMenu() {
-    document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('menu-expanded');
 }
 
 ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
 }).reveal(`
 #home, 
 #home img, 
@@ -47,5 +85,4 @@ ScrollReveal({
 #services .cards,
 #about,
 #about header,
-#about .content`)
-
+#about .content`);
